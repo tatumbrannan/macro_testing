18a3a6db286c3df1ffc3b6c8399d712ef0407191
tatumb <tatumb@ucr.edu>
2024-04-15 16:09:19 -0400

    testing

---
+++ models.sql/employees.sql
@@ -0,0 +1,17 @@
+select * from macro_testing.models.ls_access_history;
+
+WITH query_history_cte AS (
+    SELECT
+        MAX(end_time) AS last_usage,
+        COUNT(*) AS query_count
+    FROM macro_testing.INFORMATION_SCHEMA.QUERY_HISTORY
+    WHERE query_text ILIKE '%' || '{{ in_database }}' || '.' || '{{ in_schema }}' || '.' || '{{ in_object }}' || '%'
+)
+
+SELECT
+    last_usage,
+    query_count,
+    COUNT(*) AS unused_data_count
+FROM query_history_cte
+JOIN {{ in_database }}.{{ in_schema }}.{{ in_object }} ON last_accessed_timestamp < CURRENT_TIMESTAMP() - {{ in_time_interval }}
+;
